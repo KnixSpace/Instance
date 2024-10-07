ff0489f4a3e52500bfcc8a701b321fdcc92cafeb
Prakash Bhukan <prakashbhukan95@gmail.com>
2024-10-07 14:56:30 +0530

    refactor-linkedin-integration

--- server/models/Linkedin.js
+++ server/models/Linkedin.js
@@ -6,7 +6,7 @@ const linkedinAccountSchema = new mongoose.Schema({
         ref:"User",
         required:true, 
     },
-    intigrationId:{
+    integrationId:{
         type: mongoose.Schema.Types.ObjectId,
         ref:"Integration",
         required: true,
@@ -15,8 +15,8 @@ const linkedinAccountSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     accountId: { type: String, required: true },
     accessToken: { type: String, required: true },
-    refreshToken: { type: String, required: true },
+    //refreshToken: { type: String, required: true },
 });
 
 const Linkedin = mongoose.model("linkedin",linkedinAccountSchema);
-exports.LinkedIn = Linkedin;
+exports.Linkedin = Linkedin;
