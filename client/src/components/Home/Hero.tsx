6036ef58b39f7ea086d8e0c324c076e1233594d1
MihirBhavsar3102 <mihirbhavsar1995@gmail.com>
2024-10-08 22:42:11 +0530

    Frontend Landing page Done

--- client/src/components/Home/Hero.tsx
+++ client/src/components/Home/Hero.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+"use client"; // Ensures this component runs on the client side
 
-const Hero = () => {
+import React from "react";
+
+export const Hero = () => {
   return (
-    <div>
-      HeroComponent
-    </div>
-  )
-}
-
-export default Hero
+    <main className="flex flex-col items-center justify-center text-center px-6 sm:px-8 h-[50vh] md:h-[60vh]">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">INSTANCE</h1>
+      <p className="text-lg sm:text-xl text-gray-400 mt-4">
+        Automate. Simplify. Succeed.
+      </p>
+      <button className="mt-6 py-2 sm:py-3 px-6 sm:px-8 bg-indigo-600 text-white rounded-lg text-lg hover:bg-indigo-700">
+        Let's Automate
+      </button>
+    </main>
+  );
+};
