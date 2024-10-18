c8047828df7f0ff873e0047b6c0dbe0f4124a330
Krupal Patel <krupalgp2003@gmail.com>
2024-10-19 02:30:43 +0530

    added get all drive files, and get file meta data services of drive

--- server/controllers/google/services.js
+++ server/controllers/google/services.js
@@ -1,9 +1,59 @@
 const { google } = require("googleapis");
-const { oauth2Client, isTokenExpired, updateTokens } = require("./config");
 const { Google } = require("../../models/Google");
+const { oauth2Client, isTokenExpired, updateTokens } = require("./config");
 
 const sheets = google.sheets("v4");
 const drive = google.drive("v3");
+
+//drive services
+async function getAllDriveFiles(userId, email) {
+  const account = await Google.findOne({ userId, email });
+
+  if (!account) {
+    throw new Error("No Google account found");
+  }
+
+  let access_token = account.tokens.accessToken;
+
+  if (isTokenExpired(account.tokens.expiry)) {
+    const tokens = await updateTokens(account);
+    access_token = tokens.access_token;
+  }
+
+  oauth2Client.setCredentials({ access_token });
+
+  const response = await drive.files.list({
+    auth: oauth2Client,
+    fields: "files(id, name, mimeType, webViewLink, modifiedTime)",
+  });
+
+  return response.data.files;
+}
+
+async function getFileMetadata(fileId, userId, email) {
+  const account = await Google.findOne({ userId, email });
+
+  if (!account) {
+    throw new Error("No Google account found");
+  }
+
+  let access_token = account.tokens.accessToken;
+
+  if (isTokenExpired(account.tokens.expiry)) {
+    const tokens = await updateTokens(account);
+    access_token = tokens.access_token;
+  }
+
+  oauth2Client.setCredentials({ access_token });
+
+  const response = await drive.files.get({
+    auth: oauth2Client,
+    fileId,
+    fields: "id, name, mimeType, size, webViewLink, modifiedTime",
+  });
+
+  return response.data;
+}
 
 async function getAllSpreadsheet(userId, email) {
   const account = await Google.findOne({ userId, email });
@@ -82,6 +132,8 @@ async function getNewEntryOfSheet(
 }
 
 module.exports = {
+  getAllDriveFiles,
+  getFileMetadata,
   getAllSpreadsheet,
   getAllSpreadsheetSheets,
   getNewEntryOfSheet,
