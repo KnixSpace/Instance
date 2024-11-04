a7670ea28721e5f916e4c638db82aa80a4016494
Krupal Patel <krupalgp2003@gmail.com>
2024-11-04 14:01:03 +0530

    refactor: update ActionNode and TriggerNode components to support selection state and improve styling

--- client/src/components/workflow/nodes/TriggerNode.tsx
+++ client/src/components/workflow/nodes/TriggerNode.tsx
@@ -1,18 +1,21 @@
-import { Handle, Position } from "@xyflow/react";
+import { Handle, NodeProps, Position } from "@xyflow/react";
 
-type Props = {
-  data: any;
-};
-const TriggerNode = ({ data }: Props) => {
+const TriggerNode = ({ data, selected }: NodeProps) => {
   return (
     <>
-      <div className="bg-background border border-secondary p-4 flex gap-4 items-center rounded-sm text-gray-200 max-w-64">
-        <img src={data.icon} alt="" className="size-6" />
+      <div
+        className={`bg-background border border-secondary p-4 flex gap-4 items-center rounded-md text-gray-200 max-w-64 transition-all duration-300 ease-in-out ${
+          selected ? "border-cta" : ""
+        }`}
+      >
+        <img src={data.icon as string} alt="" className="size-6" />
         <div className="flex-1">
-          <h2 className="font-medium text-sm mb-1">{data.label}</h2>
-          <p className="text-xs text-gray-400 text-wrap">{data.description}</p>
+          <h2 className="font-medium text-sm mb-1">{data.label as string}</h2>
+          <p className="text-xs text-gray-400 text-wrap">
+            {data.description as string}
+          </p>
         </div>
-        <div className="absolute top-2 right-2 size-1 rounded-full bg-green-500" />
+        <div className="absolute top-2 left-2 size-1 rounded-full bg-green-500" />
       </div>
       <Handle type="source" position={Position.Bottom} id="trigger-output" />
     </>
