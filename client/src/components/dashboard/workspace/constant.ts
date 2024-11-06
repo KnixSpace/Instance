b322ffe54498941619f5322b632a61578eed1420
Krupal Patel <krupalgp2003@gmail.com>
2024-11-06 23:24:03 +0530

    feat: add action configuration for GitHub and Google services with dynamic fields and validation

--- client/src/components/dashboard/workspace/constant.ts
+++ client/src/components/dashboard/workspace/constant.ts
@@ -1,4 +1,6 @@
+import * as Yup from "yup";
 import { appIcons } from "@/lib/constants";
+import { ActionConfig } from "@/types/configurationTypes";
 
 export const workflowNodesConfig = [
   {
@@ -88,5 +90,266 @@ export const workflowNodesConfig = [
       description: "Share a post on LinkedIn",
       icon: appIcons.linkedin,
     },
+  },
+];
+
+export const actionConfig: ActionConfig[] = [
+  {
+    action: "NEW_COMMIT",
+    service: "Github",
+    icon: appIcons.github,
+    configFields: [
+      {
+        name: "repoName",
+        label: "Repository Name",
+        type: "select",
+        placeholder: "Select a repository",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://api.github.com/user/repos",
+          headers: {}, // Add headers if required
+        },
+        allowedCustomInput: false,
+        validation: Yup.string().required("Repository Name is required"),
+      },
+      {
+        name: "event",
+        label: "Event",
+        type: "select",
+        placeholder: "Select an event",
+        isDynamic: false,
+        options: [
+          { label: "New Commit", value: "NEW_COMMIT" },
+          { label: "New Issue", value: "NEW_ISSUE" },
+          { label: "New Pull Request", value: "NEW_PULL_REQUEST" },
+        ],
+        allowedCustomInput: false,
+        validation: Yup.string().required("Event is required"),
+      },
+    ],
+    outputFields: [
+      {
+        name: "commitId",
+        icon: appIcons.github,
+      },
+      {
+        name: "commitMessage",
+        icon: appIcons.github,
+      },
+      {
+        name: "commitAuthor",
+        icon: appIcons.github,
+      },
+    ],
+  },
+  {
+    action: "SHEET_NEW_ENTRY",
+    service: "Google",
+    icon: appIcons.sheets,
+    configFields: [
+      {
+        name: "sheetId",
+        label: "Sheet ID",
+        type: "select",
+        placeholder: "Select a sheet",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://sheets.googleapis.com/v4/spreadsheets",
+          headers: {},
+        },
+        allowedCustomInput: false,
+        validation: Yup.string().required("Sheet ID is required"),
+      },
+      {
+        name: "range",
+        label: "Range",
+        type: "text",
+        placeholder: "Enter range SHEET_NAME!A1:Z100",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string()
+          .required("Range is required")
+          .matches(/^[A-Za-z0-9_!]+$/, "Invalid range"),
+      },
+    ],
+    outputFields: [
+      {
+        name: "values",
+        icon: appIcons.sheets,
+      },
+      {
+        name: "sheet link",
+        icon: appIcons.sheets,
+      },
+    ],
+  },
+  {
+    action: "CREATE_DOC",
+    service: "Google",
+    icon: appIcons.docs,
+    configFields: [
+      {
+        name: "filename",
+        label: "Filename",
+        type: "select",
+        placeholder: "Select a filename",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string()
+          .trim()
+          .required("Filename is required")
+          .min(3, "Filename must be at least 3 characters"),
+      },
+      {
+        name: "folderId",
+        label: "Folder ID",
+        type: "select",
+        placeholder: "Select a folder",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://drive.googleapis.com/v3/files",
+          headers: {},
+        },
+        allowedCustomInput: false,
+        validation: Yup.string(),
+      },
+    ],
+    outputFields: [
+      {
+        name: "docId",
+        icon: appIcons.docs,
+      },
+      {
+        name: "doc link",
+        icon: appIcons.docs,
+      },
+    ],
+  },
+  {
+    action: "CREATE_SHEET",
+    service: "Google",
+    icon: appIcons.sheets,
+    configFields: [
+      {
+        name: "filename",
+        label: "Filename",
+        type: "select",
+        placeholder: "Select a filename",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string()
+          .trim()
+          .required("Filename is required")
+          .min(3, "Filename must be at least 3 characters"),
+      },
+      {
+        name: "folderId",
+        label: "Folder ID",
+        type: "select",
+        placeholder: "Select a folder",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://drive.googleapis.com/v3/files",
+          headers: {},
+        },
+        allowedCustomInput: false,
+        validation: Yup.string(),
+      },
+    ],
+    outputFields: [
+      {
+        name: "sheetId",
+        icon: appIcons.sheets,
+      },
+      {
+        name: "sheet link",
+        icon: appIcons.sheets,
+      },
+    ],
+  },
+  {
+    action: "APPEND_ROW",
+    service: "Google",
+    icon: appIcons.sheets,
+    configFields: [
+      {
+        name: "sheetId",
+        label: "Sheet ID",
+        type: "select",
+        placeholder: "Select a sheet",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://sheets.googleapis.com/v4/spreadsheets",
+          headers: {},
+        },
+        allowedCustomInput: false,
+        validation: Yup.string().required("Sheet ID is required"),
+      },
+      {
+        name: "range",
+        label: "Range",
+        type: "text",
+        placeholder: "Enter range SHEET_NAME!A1:Z100",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string()
+          .required("Range is required")
+          .matches(/^[A-Za-z0-9_!]+$/, "Invalid range"),
+      },
+      {
+        name: "values",
+        label: "Values",
+        type: "text",
+        placeholder: "Enter values",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string().required("Values are required"),
+      },
+    ],
+    outputFields: [
+      {
+        name: "updatedRange",
+        icon: appIcons.sheets,
+      },
+      {
+        name: "updatedValues",
+        icon: appIcons.sheets,
+      },
+    ],
+  },
+  {
+    action: "APPEND_TEXT",
+    service: "Google",
+    icon: appIcons.docs,
+    configFields: [
+      {
+        name: "docId",
+        label: "Doc ID",
+        type: "select",
+        placeholder: "Select a document",
+        isDynamic: true,
+        dynamicOptions: {
+          url: "https://drive.googleapis.com/v3/files",
+          headers: {},
+        },
+        allowedCustomInput: false,
+        validation: Yup.string().required("Doc ID is required"),
+      },
+      {
+        name: "text",
+        label: "Text",
+        type: "select",
+        placeholder: "Select a text",
+        isDynamic: false,
+        allowedCustomInput: true,
+        validation: Yup.string().required("Text is required"),
+      },
+    ],
+    outputFields: [
+      {
+        name: "updatedText",
+        icon: appIcons.docs,
+      },
+    ],
   },
 ];
