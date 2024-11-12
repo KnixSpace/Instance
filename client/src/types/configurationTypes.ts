5135ba54d088266971c6258fe8642ecf78ffe5a0
Krupal Patel <krupalgp2003@gmail.com>
2024-11-12 18:41:25 +0530

    feat: implement useNodeConfiguration hook for dynamic form options and node management

--- client/src/types/configurationTypes.ts
+++ client/src/types/configurationTypes.ts
@@ -1,3 +1,4 @@
+import { UseFormReturn } from "react-hook-form";
 import * as Yup from "yup";
 
 export interface ConfigField {
@@ -21,4 +22,11 @@ export interface ActionConfig {
   icon: string;
   configFields: ConfigField[];
   outputFields: string[];
+}
+
+export interface ConfigurationFieldProps {
+  field: ConfigField;
+  control: UseFormReturn<any>["control"];
+  errors: UseFormReturn<any>["formState"]["errors"];
+  dynamicOptions: { label: string; value: string }[] | undefined;
 }
