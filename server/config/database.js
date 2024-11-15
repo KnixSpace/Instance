2afcf51161079904410a9e0aeb840449ffd32dfc
BHADRIKA <bhadrika2226@gmail.com>
2024-11-15 19:12:52 +0530

    Refactor scheduler logic: move to engine, implement change stream, and update nextRun handling

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
+const {startChangeStream} = require("../engine/scheduler");
 
 const connectDB = async () => {
   try {
+    
+    mongoose.connection.once('open', async() => {
+      // console.error("MongoDB error:");
+      startChangeStream();
+    });
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDb connected on ${mongoose.connection.host}`);
+
   } catch (error) {
     console.error("Database connection error:", error);
     process.exit(1);
   }
-};
 
+};
 module.exports = connectDB;
