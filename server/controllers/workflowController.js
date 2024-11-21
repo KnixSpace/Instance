b72e0f1666759c4a244ba2accaa0095c5818093b
BHADRIKA <bhadrika2226@gmail.com>
2024-11-22 01:14:55 +0530

    Implement workflow update and status management functions; add routes for updating metadata and fetching workflow details.

--- server/controllers/workflowController.js
+++ server/controllers/workflowController.js
@@ -48,39 +48,95 @@ function topologicalSort(graph) {
 
 //WIP: Implement the updateWorkflow function
 async function updateWorkflow(req, res) {
-  const { adjacencyList } = req.body;
+  const { adjacencyList, workflowId } = req.body;
 
   if (!adjacencyList) {
     return res.status(400).json({ message: "Adjacency list is required." });
   }
 
   const executionOrder = topologicalSort(adjacencyList);
+  try {
+    const workflow = await WorkFlow.findByIdAndUpdate(workflowId, {
+      nodes: req.body.nodes,
+      edges: req.body.edges,
+      executionOrder: executionOrder,
+    }, { $inc: { version: 1 } }, { new: true });
 
-  const workflow = new WorkFlow({
-    name: req.body.name,
-    description: req.body.description,
-    version: 1,
-    status: "draft",
-    nodes: req.body.nodes,
-    edges: req.body.edges,
-    executionOrder: executionOrder,
-    metadata: {
-      createdBy: req.body.metadata.createdBy,
-    },
-  });
-  // console.log(adjacencyList);
-  // console.log(executionOrder);
-  // res.status(200).json({ message: "Workflow created successfully" });
-  res.json(await workflow.save());
+    if (!workflow) {
+      return res.status(404).json({ message: "Workflow not found" });
+    }
+
+    res.status(200).json(workflow);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating workflow" });
+  }
 }
 
-//WIP: Implement the status active update function
+//Implement the status active update function
+async function statusUpdate(req, res) {
+  const { workflowId, status } = req.body;
 
-//WIP: Implement the workflow description update function
+  try {
+    let workflow = await WorkFlow.findById(workflowId);
+    if (status === "active") {
+      workflow.status = "inactive";
+      workflow = await workflow.save();
+      return res.status(400).json({ status: workflow.status });
+    }
+    else {
+      workflow.status = "active";
+      workflow = await workflow.save();
+      return res.status(200).json({ status: workflow.status });
+    }
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Error updating workflow status" });
+  }
+};
 
-//WIP: get all the workflows (title, description, status,id)
+//Implement the workflow description update function
+async function updateMetaData(req, res) {
+  const { workflowId, description, name } = req.body;
+  try {
+    let workflow = await WorkFlow.findByIdAndUpdate(workflowId, { $set: { description: description, name: name } }, { new: true });
+    console.log("workflow", workflow);
+    return res.status(200).json({ name: workflow.name, description: workflow.description });
+  } catch (error) {
+    return res.status(500).json({ message: "Error updating workflow title and description" });
+  }
+}
 
-//WIP: get all the details of a workflow
+// get all the workflows (title, description, status,id)
+async function getWorkflowSummary(req, res) {
+  const { workflowId } = req.body;
+  try {
+    const workflows = await WorkFlow.findById(workflowId);
+    const summary = {
+      workflowId,
+      name: workflows.name,
+      description: workflows.description,
+      status: workflows.status
+    };
+    res.status(200).json(summary);
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Error fetching workflows" });
+  }
+
+}
+
+//get all the details of a workflow
+
+async function getWorkflowdetails(req, res) {
+  const { workflowId } = req.body;
+  try {
+    const workflows = await WorkFlow.findById(workflowId);
+    res.status(200).json(workflows);
+  }
+  catch (error) {
+    return res.status(500).json({ message: "Error fetching workflows" });
+  }
+}
 
 async function fetchServiceAccount(req, res) {
   try {
@@ -97,8 +153,8 @@ async function fetchServiceAccount(req, res) {
       match:
         service === "google" && scopes?.length > 0
           ? {
-              scopes: { $all: scopes },
-            }
+            scopes: { $all: scopes },
+          }
           : {},
       select: "email name avatar",
     });
@@ -134,4 +190,4 @@ async function createWorkflow(req, res) {
   }
 }
 
-module.exports = { createWorkflow, fetchServiceAccount, updateWorkflow };
+module.exports = { createWorkflow, fetchServiceAccount, updateWorkflow, statusUpdate, getWorkflowSummary, getWorkflowdetails, updateMetaData };
