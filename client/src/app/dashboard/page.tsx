84f1e190d478b24d35da96f4b48062381dae1de2
Krupal Patel <krupalgp2003@gmail.com>
2024-12-04 17:45:21 +0530

    Enhance Dashboard UI; replace header text, add welcome message with animated effects, and improve workflow display logic for better user experience.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { AnimatedShinyText } from "@/components/ui/AnimatedShinyText";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -20,7 +22,6 @@ const Dashboard = (props: Props) => {
         `${process.env.NEXT_PUBLIC_BASE_URL}/workflow/getAllWorkflows`,
         { withCredentials: true }
       );
-      console.log(response.data);
       if (response.status === 200) {
         setWorkflows(response.data);
       }
@@ -36,41 +37,68 @@ const Dashboard = (props: Props) => {
   return (
     <>
       <section className="flex flex-col h-dvh w-full divide-y divide-darkSecondary">
-        <div className="flex items-center p-4 text-lg px-4">Workflows</div>
+        <div className="flex items-center p-4 text-lg px-4">Dashboard</div>
         <div className="py-4 px-4 flex-1 overflow-y-auto overflow-x-hidden">
-          <div>
-            <div className="w-full grid grid-cols-3 gap-4">
-              {workflows.map((workflow) => (
-                <Link
-                  key={workflow.workflowId}
-                  href={`/dashboard/workflow/${workflow.workflowId}`}
-                  className="border border-lightbackground hover:border-secondary rounded-md p-4"
-                >
-                  <div className="flex gap-4 items-center mb-2">
-                    <h1 className="text-base flex-1">{workflow.name}</h1>
-                    <span
-                      className={`text-xs p-1 px-2 rounded-md ${
-                        workflow.status === "active"
-                          ? "bg-green-500"
-                          : workflow.status === "inactive"
-                          ? "bg-secondary"
-                          : "bg-lightbackground"
-                      }`}
-                    >
-                      {workflow.status === "active"
-                        ? "Active"
-                        : workflow.status === "inactive"
-                        ? "Inactive"
-                        : "Draft"}
-                    </span>
-                  </div>
-                  <p className="text-sm text-secondary">
-                    {workflow.description}
-                  </p>
-                </Link>
-              ))}
+          {workflows.length === 0 ? (
+            <div className="h-full flex flex-col justify-center items-center overflow-auto">
+              <div className="mb-8 flex items-center flex-col">
+                <div className="mb-1">
+                  <TextGenerateEffect
+                    words="Welcome to Instance"
+                    className="font-semibold text-6xl text-gray-200"
+                  />
+                </div>
+                <AnimatedShinyText className="w-full" shimmerWidth={200}>
+                  <span className="text-lg font-semibold">
+                    Effortlessly Build and Automate Your Workflows!
+                  </span>
+                </AnimatedShinyText>
+              </div>
+              <Link
+                href={"/dashboard/workflow/new"}
+                className="relative flex justify-center items-center overflow-hidden rounded-full p-px"
+              >
+                <div className="z-10 flex items-center justify-center px-8 py-2 bg-background rounded-full h-full w-full">
+                  Let's Automate
+                </div>
+                <span className="absolute inset-[-1000%] animate-[spin_1s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#7441FE_0%,#E3D9FE_50%,#7441FE_100%)]" />
+              </Link>
             </div>
-          </div>
+          ) : (
+            <div>
+              <div className="w-full grid grid-cols-3 gap-4">
+                {workflows.map((workflow) => (
+                  <Link
+                    key={workflow.workflowId}
+                    href={`/dashboard/workflow/${workflow.workflowId}`}
+                    className="border border-lightbackground hover:border-secondary rounded-md p-4"
+                  >
+                    <div className="flex gap-4 items-center mb-2">
+                      <h1 className="text-base flex-1">{workflow.name}</h1>
+                      <span
+                        className={`text-xs p-1 px-2 rounded-md ${
+                          workflow.status === "active"
+                            ? "bg-green-500"
+                            : workflow.status === "inactive"
+                            ? "bg-secondary"
+                            : "bg-lightbackground"
+                        }`}
+                      >
+                        {workflow.status === "active"
+                          ? "Active"
+                          : workflow.status === "inactive"
+                          ? "Inactive"
+                          : "Draft"}
+                      </span>
+                    </div>
+                    <p className="text-sm text-secondary">
+                      {workflow.description}
+                    </p>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </>
