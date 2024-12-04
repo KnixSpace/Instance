c64710c8f93f0b3a89ee5860baf44a0e65aaf70d
Krupal Patel <krupalgp2003@gmail.com>
2024-12-04 18:26:29 +0530

    Add application icons to Dashboard; display Google, GitHub, LinkedIn, and Notion icons for enhanced visual integration and user engagement.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { AnimatedShinyText } from "@/components/ui/AnimatedShinyText";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { appIcons } from "@/lib/constants";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -23,6 +24,7 @@ const Dashboard = (props: Props) => {
         { withCredentials: true }
       );
       if (response.status === 200) {
+        console.log(response.data);
         setWorkflows(response.data);
       }
     } catch (error) {
@@ -56,13 +58,34 @@ const Dashboard = (props: Props) => {
               </div>
               <Link
                 href={"/dashboard/workflow/new"}
-                className="relative flex justify-center items-center overflow-hidden rounded-full p-px"
+                className="relative flex justify-center items-center overflow-hidden rounded-full p-px mb-12"
               >
                 <div className="z-10 flex items-center justify-center px-8 py-2 bg-background rounded-full h-full w-full">
                   Let's Automate
                 </div>
                 <span className="absolute inset-[-1000%] animate-[spin_1s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#7441FE_0%,#E3D9FE_50%,#7441FE_100%)]" />
               </Link>
+              <div className="flex flex-col items-center">
+                <p className="mb-6 text-gray-300">
+                  Wide range of applictions to automate your works
+                </p>
+                <div className="flex justify-center">
+                  <div className="grid grid-cols-4 gap-4">
+                    <div className="size-8 object-contain">
+                      <img src={appIcons.google} alt="" />
+                    </div>
+                    <div className="size-8 object-contain">
+                      <img src={appIcons.github} alt="" />
+                    </div>
+                    <div className="size-8 object-contain">
+                      <img src={appIcons.linkedin} alt="" />
+                    </div>
+                    <div className="size-8 object-contain">
+                      <img src={appIcons.notion} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           ) : (
             <div>
